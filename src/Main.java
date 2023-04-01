9b40bb0a851968662b326820e7d9db56d7d7eda0
Abhinav Kumar <kumaraka2000@gmail.com>
2023-04-01 18:06:26 +0530

    Initial Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		List<Integer> numbersList;
+		numbersList = Arrays.asList(10, 11, 12, 13, 14, 15);
+		//Using Collectors
+		List<Integer> evenNumbersList = numbersList.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+		System.out.println(evenNumbersList);
+		//Using forEach
+		numbersList.stream().filter(n -> n % 2 == 0).forEach(System.out::println/*'n -> System.out.println' will also work.*/);
+	}
 }
